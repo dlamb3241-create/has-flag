66734fe3a1f7782c93a66428fb5d4939cd467585
MoneyWiseGuide <lambdavid985@gmail.com>
2026-01-05 09:01:29 -0500

    Add edge case tests

--- test.js
+++ test.js
@@ -32,3 +32,21 @@ test('throws on non-string flag', t => {
 	}, {instanceOf: TypeError});
 });
 
+test('handles empty argv', t => {
+	t.false(hasFlag('foo', []));
+});
+
+test('detects flag at first position', t => {
+	t.true(hasFlag('foo', ['--foo']));
+});
+
+test('ignores flags after --', t => {
+	t.false(hasFlag('foo', ['--', '--foo']));
+});
+
+test('throws on non-string flag', t => {
+	t.throws(() => {
+		hasFlag();
+	}, {instanceOf: TypeError});
+});
+
